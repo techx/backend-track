ca93b240cbca368abbb166d2bbc15db1c65a933d
Bryan Chen <bchen21@mit.edu>
2020-02-06 13:22:00 -0500

    auth and time

--- demo/app.py
+++ demo/app.py
@@ -1,21 +1,52 @@
 from pymongo import MongoClient
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import datetime
 
 app = Flask(__name__)
 app.config.from_json('config.json')
+app.secret_key = app.config['SECRET_KEY']
 
 client = MongoClient(app.config['MONGO_URI'])
 db = client['blueprint']
 messages = db['messages']
+users = db['users']
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html', messages=messages.find({}))
+    return render_template('index.html', messages=messages.find({}), loggedIn='username' in session, username=session.get('username', ''))
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    if (users.count_documents({'username': username}) > 0):
+        return 'failure'
+    users.insert_one({'username': username, 'password': password})
+    session['username'] = username
+    return 'success'
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    if (users.count_documents({'username': username, 'password': password}) > 0):
+        session['username'] = username
+        return 'success'
+    return 'failure'
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('username', None)
+    return 'success'
 
 @app.route('/message', methods=['POST'])
 def message():
+    if 'username' not in session:
+        return 'failure'
+    username = session['username']
     message = request.form['message']
-    messages.insert_one({'message': message})
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    messages.insert_one({'username': username, 'message': message, 'ts': now})
     return 'success'
 
 if __name__ == '__main__':
--- demo/static/main.js
+++ demo/static/main.js
@@ -1,8 +1,37 @@
 $(document).ready(function() {
+    $('#register').click(function() {
+        const username = $('#regusername').val();
+        const password = $('#regpassword').val();
+        const confirmPassword = $('#regconfirmpassword').val();
+        if (password != confirmPassword) {
+            return;
+        }
+        $.post('/register', {
+            username: username,
+            password: password,
+        }).done(function() {
+            document.location.reload();
+        });
+    })
+    $('#login').click(function() {
+        const username = $('#username').val();
+        const password = $('#password').val();
+        $.post('/login', {
+            username: username,
+            password: password,
+        }).done(function() {
+            document.location.reload();
+        });
+    });
+    $('#logout').click(function() {
+        $.post('/logout', {}).done(function() {
+            document.location.reload();
+        });
+    });
     $('#submit').click(function() {
         const message = $('#message').val();
         $.post('/message', {
-            message: message
+            message: message,
         }).done(function() {
             document.location.reload();
         });
